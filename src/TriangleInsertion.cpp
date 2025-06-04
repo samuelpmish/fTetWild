a09fc7e49cb013536b77a14396950be34faeaa6d
Sam Mish <samuelpmish@gmail.com>
2025-06-03 20:57:55 -0700

    remove deprecated std::random_shuffle

--- src/TriangleInsertion.cpp
+++ src/TriangleInsertion.cpp
@@ -36,6 +36,7 @@
 
 #include <bitset>
 #include <numeric>
+#include <algorithm> // for std::random_shuffle
 #include <unordered_map>
 
 
@@ -140,7 +141,10 @@ void floatTetWild::sort_input_faces(const std::vector<Vector3> &input_vertices, 
     if (mesh.params.not_sort_input)
         return;
 
-    std::random_shuffle(sorted_f_ids.begin(), sorted_f_ids.end());
+    static std::random_device rd;
+    static std::mt19937 g(rd());
+
+    std::shuffle(sorted_f_ids.begin(), sorted_f_ids.end(), g);
 //    std::sort(sorted_f_ids.begin(), sorted_f_ids.end(), [&weights](int a, int b) {
 //        return weights[a] < weights[b];
 //    });
